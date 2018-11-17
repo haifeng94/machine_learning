44cf335dd1ff5c8b173642046dc0ea63bb70ce8e
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-11-17 08:59:17 +0800

    Titanic solution 1

---
+++ Kaggle_Titanic/titanic_1/Titanic.py
@@ -0,0 +1,71 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn import model_selection
+
+
+# 线性回归
+def linearRegress(data_train):
+    # 数值处理，Age列缺失值使用中位数填充
+    data_train = data_train
+    data_train['Age'] = data_train['Age'].fillna(data_train['Age'].median())
+
+    # 提取简单的特征(主要提取了数值型特征)
+    predictors = ["Pclass","Age","SibSp","Parch","Fare"]
+
+    # 初始化算法
+    lr = LinearRegression()
+
+    # 样本平均分成3份，3折交叉验证
+    kf = KFold(n_splits=3, shuffle=False, random_state=1)
+
+    predictions = []
+    for train,test in kf.split(data_train):
+        train_predictors = data_train[predictors].iloc[train,:]
+        train_target = data_train['Survived'].iloc[train]
+        lr.fit(train_predictors,train_target)
+
+        test_predictions = lr.predict(data_train[predictors].iloc[test,:])
+        predictions.append(test_predictions)
+
+    # array聚合
+    predictions = np.concatenate(predictions,axis=0)
+
+    predictions[predictions > .5] = 1
+    predictions[predictions <= .5] = 0
+
+    accuracy = sum(predictions == data_train["Survived"]) / len(predictions)
+    print("准确率为: ", accuracy)
+
+# 逻辑回归
+def logRegress(data_train):
+    # 数值处理，Age列缺失值使用中位数填充
+    data_train = data_train
+    data_train['Age'] = data_train['Age'].fillna(data_train['Age'].median())
+
+    # 提取简单的特征(主要提取了数值型特征)
+    predictors = ["Pclass", "Age", "SibSp", "Parch", "Fare"]
+
+    # 初始化逻辑回归
+    logReg = LogisticRegression(random_state=1)
+
+    logReg.fit(data_train[predictors], data_train['Survived'])
+
+    # 使用sklearn库里面的交叉验证函数获取预测准确率分数
+    scores = model_selection.cross_val_score(logReg,data_train[predictors],data_train['Survived'],cv=3)
+
+    # 使用交叉验证分数的平均值作为最终的准确率
+    print("准确率为: ", scores.mean())
+
+if __name__ == "__main__":
+    data_train = pd.read_csv('train.csv')
+    data_test = pd.read_csv('test.csv')
+
+    # 线性回归简单处理
+    #linearRegress(data_train)
+
+    # 逻辑回归简单处理
+    logRegress(data_train)
---
+++ Kaggle_Titanic/titanic_1/Titanic2.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+
+# 数据处理
+def dataProcess(data):
+    # Age列中的缺失值用Age中位数进行填充
+    data['Age'] = data['Age'].fillna(data['Age'].median())
+
+    # Fare列中的缺失值用Fare最大值进行填充
+    data['Fare'] = data['Fare'].fillna(data['Fare'].max())
+
+    # Embarked缺失值用最多的S进行填充
+    data['Embarked'] = data['Embarked'].fillna('S')
+    # Embarked用0,1,2代替S,C,Q
+    data.loc[data['Embarked'] == 'S', 'Embarked'] = 0
+    data.loc[data['Embarked'] == 'C', 'Embarked'] = 1
+    data.loc[data['Embarked'] == 'Q', 'Embarked'] = 2
+
+    # Sex性别列处理：male用0，female用1
+    data.loc[data['Sex'] == 'male', 'Sex'] = 0
+    data.loc[data['Sex'] == 'female','Sex'] =1
+
+    return data
+
+# 逻辑回归
+def logRegress(train,test):
+    data_train = dataProcess(train)
+    data_test = dataProcess(test)
+    predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+    logReg = LogisticRegression(random_state=1)
+    logReg.fit(data_train[predictors], data_train['Survived'])
+
+    scores = model_selection.cross_val_score(logReg,data_train[predictors],data_train['Survived'],cv=3)
+    print("准确率为: ", scores.mean())
+
+    # 构造测试集的Survived列
+    data_test['Survived'] = -1
+
+    data_test['Survived'] = logReg.predict(data_test[predictors])
+    print(data_test['Survived'].head(10))
+
+    # 保存为csv文档
+    result = pd.DataFrame({'PassengerId': data_test['PassengerId'], 'Survived': data_test['Survived']})
+    result.to_csv('result.csv', index=False, sep=',')
+
+def randomForest(train,test):
+    data_train = dataProcess(train)
+    data_test = dataProcess(test)
+
+    predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+    # 30棵决策树，停止的条件：样本个数为2，叶子节点个数为1
+    rf = RandomForestClassifier(random_state=1,n_estimators=30,min_samples_split=2,min_samples_leaf=1)
+
+    # 样本平均分成10份，10折交叉验证
+    kf = KFold(n_splits=10,shuffle=False,random_state=1)
+
+    rf.fit(data_train[predictors],data_train['Survived'])
+
+    scores = model_selection.cross_val_score(rf,data_train[predictors],data_train['Survived'],cv=kf)
+    print(scores)
+    print(scores.mean())
+
+    # 构造测试集的Survived列
+    data_test['Survived'] = -1
+
+    data_test['Survived'] = rf.predict(data_test[predictors])
+    print(data_test['Survived'].head())
+
+    # 保存为csv文档
+    result = pd.DataFrame({'PassengerId': data_test['PassengerId'], 'Survived': data_test['Survived']})
+    result.to_csv('result.csv', index=False, sep=',')
+
+if __name__ == "__main__":
+    data_train = pd.read_csv('train.csv')
+    data_test = pd.read_csv('test.csv')
+
+    # 逻辑回归
+    #logRegress(data_train,data_test)
+
+    # 随机森林
+    randomForest(data_train,data_test)
