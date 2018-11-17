d30462410f27faf0e11ec3634928bc005f90c9f5
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2018-11-17 09:18:01 +0800

    Titanic solution 2

--- Kaggle_Titanic/titanic_2/titanic.py
+++ Kaggle_Titanic/titanic_2/titanic.py
@@ -18,7 +18,7 @@ from xgboost import XGBClassifier
 此代码主要包括特征工程处理、模型融合、数据预测、及生成预测结果(StackingSubmission.csv)
 '''
 
-# 特征提取
+#特征提取
 def get_top_n_features(train_data_X,train_data_Y,top_n_features):
     # randomforest
     rf = RandomForestClassifier(random_state=0)
