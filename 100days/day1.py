c46f308f03f29c7889f6ef24eacb5e52c20b757f
haifeng_guan <37890295+haifeng94@users.noreply.github.com>
2019-01-24 19:55:04 +0800

    Data PreProcessing | Day 1

--- 100days/day1.py
+++ 100days/day1.py
@@ -13,13 +13,12 @@ def pre_process():
     dataset = pd.read_csv('Data.csv')
     X = dataset.iloc[ : , :-1].values
     Y = dataset.iloc[ : , 3].values
-    #print(X)
-    #print(Y)
+    
     #Handling the missing data
     imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
     imputer = imputer.fit(X[ : , 1:3])
     X[ : , 1:3] = imputer.transform(X[ : , 1:3])
-    #print(X)
+   
     #Encoding categorical data
     labelencoder_X = LabelEncoder()
     labelencoder_Y = LabelEncoder()
@@ -27,10 +26,10 @@ def pre_process():
     Y = labelencoder_Y.fit_transform(Y)
     onehotencoder = OneHotEncoder(categorical_features = [0])
     X = onehotencoder.fit_transform(X).toarray()
-    #print(X)
+    
     #Splitting the datasets into training sets and Test sets
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
-    #print(X_test)
+   
     #Feature Scaling
     sc_X = StandardScaler()
     X_train = sc_X.fit_transform(X_train)
